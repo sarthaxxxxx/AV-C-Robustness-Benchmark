b1df65904425a08c13c3d3e57c7a8718febea484
Sarthak Maharana <SKM200005@utdallas.edu>
2025-02-17 18:49:43 -0600

    v3 - visual jpeg compression added

--- data_recipe/src/generate_visual_corruptions.py
+++ data_recipe/src/generate_visual_corruptions.py
@@ -38,7 +38,7 @@ IMG_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm']
 #   - Shot noise: random noise that looks like dots
 #   - Impulse noise: random noise that looks like salt and pepper
 #   - Speckle noise: random noise that looks like grains
-#   - [TODO] Compression noise: random noise due to compression artifacts
+#   - Compression noise: JPEG compression artifacts (video) + Quantization noise (audio)
 # Environmental:
 #   - Snow: random snowflakes
 #   - Frost: random frost
@@ -184,69 +184,6 @@ wandlibrary.MagickMotionBlurImage.argtypes = (ctypes.c_void_p,  # wand
 class MotionImage(WandImage):
     def motion_blur(self, radius=0.0, sigma=0.0, angle=0.0):
         wandlibrary.MagickMotionBlurImage(self.wand, radius, sigma, angle)
-
-
-# modification of https://github.com/FLHerne/mapgen/blob/master/diamondsquare.py
-def plasma_fractal(mapsize=256, wibbledecay=3):
-    """
-    Generate a heightmap using diamond-square algorithm.
-    Return square 2d array, side length 'mapsize', of floats in range 0-255.
-    'mapsize' must be a power of two.
-    """
-    assert (mapsize & (mapsize - 1) == 0)
-    maparray = np.empty((mapsize, mapsize), dtype=np.float_)
-    maparray[0, 0] = 0
-    stepsize = mapsize
-    wibble = 100
-
-    def wibbledmean(array):
-        return array / 4 + wibble * np.random.uniform(-wibble, wibble, array.shape)
-
-    def fillsquares():
-        """For each square of points stepsize apart,
-           calculate middle value as mean of points + wibble"""
-        cornerref = maparray[0:mapsize:stepsize, 0:mapsize:stepsize]
-        squareaccum = cornerref + np.roll(cornerref, shift=-1, axis=0)
-        squareaccum += np.roll(squareaccum, shift=-1, axis=1)
-        maparray[stepsize // 2:mapsize:stepsize,
-        stepsize // 2:mapsize:stepsize] = wibbledmean(squareaccum)
-
-    def filldiamonds():
-        """For each diamond of points stepsize apart,
-           calculate middle value as mean of points + wibble"""
-        mapsize = maparray.shape[0]
-        drgrid = maparray[stepsize // 2:mapsize:stepsize, stepsize // 2:mapsize:stepsize]
-        ulgrid = maparray[0:mapsize:stepsize, 0:mapsize:stepsize]
-        ldrsum = drgrid + np.roll(drgrid, 1, axis=0)
-        lulsum = ulgrid + np.roll(ulgrid, -1, axis=1)
-        ltsum = ldrsum + lulsum
-        maparray[0:mapsize:stepsize, stepsize // 2:mapsize:stepsize] = wibbledmean(ltsum)
-        tdrsum = drgrid + np.roll(drgrid, 1, axis=1)
-        tulsum = ulgrid + np.roll(ulgrid, -1, axis=0)
-        ttsum = tdrsum + tulsum
-        maparray[stepsize // 2:mapsize:stepsize, 0:mapsize:stepsize] = wibbledmean(ttsum)
-
-    while stepsize >= 2:
-        fillsquares()
-        filldiamonds()
-        stepsize //= 2
-        wibble /= wibbledecay
-
-    maparray -= maparray.min()
-    return maparray / maparray.max()
-
-
-def clipped_zoom(img, zoom_factor):
-    h = img.shape[0]
-    # ceil crop height(= crop width)
-    ch = int(np.ceil(h / zoom_factor))
-
-    top = (h - ch) // 2
-    img = scizoom(img[top:top + ch, top:top + ch], (zoom_factor, zoom_factor, 1), order=1)
-    # trim off any extra pixels
-    trim_top = (img.shape[0] - h) // 2
-
-    return img[trim_top:trim_top + h, trim_top:trim_top + h]
 
 
 # def crowd(x, severity = 1):
@@ -409,37 +346,6 @@ def underwater(x, severity = 1):
 
     return  img_hazy
 
-def glass_blur(x, severity=1):
-    # sigma, max_delta, iterations
-    c = [(0.7, 1, 2), (0.9, 2, 1), (1, 2, 3), (1.1, 3, 2), (1.5, 4, 2)][severity - 1]
-
-    x = np.uint8(gaussian(np.array(x) / 255., sigma=c[0], channel_axis=-1) * 255)
-
-    # locally shuffle pixels
-    for i in range(c[2]):
-        for h in range(224 - c[1], c[1], -1):
-            for w in range(224 - c[1], c[1], -1):
-                dx, dy = np.random.randint(-c[1], c[1], size=(2,))
-                h_prime, w_prime = h + dy, w + dx
-                # swap
-                x[h, w], x[h_prime, w_prime] = x[h_prime, w_prime], x[h, w]
-
-    return np.clip(gaussian(x / 255., sigma=c[0], channel_axis=-1), 0, 1) * 255
-
-
-def defocus_blur(x, severity=1):
-    c = [(3, 0.1), (4, 0.5), (6, 0.5), (8, 0.5), (10, 0.5)][severity - 1]
-
-    x = np.array(x) / 255.
-    kernel = disk(radius=c[0], alias_blur=c[1])
-
-    channels = []
-    for d in range(3):
-        channels.append(cv2.filter2D(x[:, :, d], -1, kernel))
-    channels = np.array(channels).transpose((1, 2, 0))  # 3x224x224 -> 224x224x3
-
-    return np.clip(channels, 0, 1) * 255
-
 
 # def motion_blur(x, severity=1):
 #     c = [(10, 3), (15, 5), (15, 8), (15, 12), (20, 15)][severity - 1]
@@ -478,29 +384,6 @@ def wind(x, severity=1):
         return np.clip(np.array([x, x, x]).transpose((1, 2, 0)), 0, 255)
 
 
-def zoom_blur(x, severity=1):
-    c = [np.arange(1, 1.11, 0.01),
-         np.arange(1, 1.16, 0.01),
-         np.arange(1, 1.21, 0.02),
-         np.arange(1, 1.26, 0.02),
-         np.arange(1, 1.31, 0.03)][severity - 1]
-
-    x = (np.array(x) / 255.).astype(np.float32)
-    out = np.zeros_like(x)
-    for zoom_factor in c:
-        out += clipped_zoom(x, zoom_factor)
-
-    x = (x + out) / (len(c) + 1)
-    return np.clip(x, 0, 1) * 255
-
-
-def fog(x, severity=1):
-    c = [(1.5, 2), (2, 2), (2.5, 1.7), (2.5, 1.5), (3, 1.4)][severity - 1]
-
-    x = np.array(x) / 255.
-    max_val = x.max()
-    x += c[0] * plasma_fractal(wibbledecay=c[1])[:224, :224][..., np.newaxis]
-    return np.clip(x * max_val / (max_val + c[0]), 0, 1) * 255
 
 def smoke(x, severity=1):
     '''Simulate smoke effect on the image - grayish appearance.'''
@@ -716,12 +599,6 @@ def spatter(x, severity=1):
         return np.clip(x + color, 0, 1) * 255
 
 
-def contrast(x, severity=1):
-    c = [0.4, .3, .2, .1, .05][severity - 1]
-
-    x = np.array(x) / 255.
-    means = np.mean(x, axis=(0, 1), keepdims=True)
-    return np.clip((x - means) * c + means, 0, 1) * 255
 
 # def crowd(x, severity=1):
 #     import numpy as np
@@ -771,19 +648,8 @@ def concert(x, severity=1):
     return np.clip(x, 0, 1) * 255
 
 
-def saturate(x, severity=1):
-    '''Simulate saturation effect on the image.'''
-    c = [(0.3, 0), (0.1, 0), (2, 0), (5, 0.1), (20, 0.2)][severity - 1]
 
-    x = np.array(x) / 255.
-    x = sk.color.rgb2hsv(x)
-    x[:, :, 1] = np.clip(x[:, :, 1] * c[0] + c[1], 0, 1)
-    x = sk.color.hsv2rgb(x)
-
-    return np.clip(x, 0, 1) * 255
-
-
-def jpeg_compression(x, severity=1):
+def compression(x, severity=1):
     c = [25, 18, 15, 10, 7][severity - 1]
 
     output = BytesIO()
@@ -793,46 +659,6 @@ def jpeg_compression(x, severity=1):
     return x
 
 
-def pixelate(x, severity=1):
-    c = [0.6, 0.5, 0.4, 0.3, 0.25][severity - 1]
-
-    x = x.resize((int(224 * c), int(224 * c)), PILImage.BOX)
-    x = x.resize((224, 224), PILImage.BOX)
-
-    return x
-
-
-# mod of https://gist.github.com/erniejunior/601cdf56d2b424757de5
-def elastic_transform(image, severity=1):
-    c = [(244 * 2, 244 * 0.7, 244 * 0.1),   # 244 should have been 224, but ultimately nothing is incorrect
-         (244 * 2, 244 * 0.08, 244 * 0.2),
-         (244 * 0.05, 244 * 0.01, 244 * 0.02),
-         (244 * 0.07, 244 * 0.01, 244 * 0.02),
-         (244 * 0.12, 244 * 0.01, 244 * 0.02)][severity - 1]
-
-    image = np.array(image, dtype=np.float32) / 255.
-    shape = image.shape
-    shape_size = shape[:2]
-
-    # random affine
-    center_square = np.float32(shape_size) // 2
-    square_size = min(shape_size) // 3
-    pts1 = np.float32([center_square + square_size,
-                       [center_square[0] + square_size, center_square[1] - square_size],
-                       center_square - square_size])
-    pts2 = pts1 + np.random.uniform(-c[2], c[2], size=pts1.shape).astype(np.float32)
-    M = cv2.getAffineTransform(pts1, pts2)
-    image = cv2.warpAffine(image, M, shape_size[::-1], borderMode=cv2.BORDER_REFLECT_101)
-
-    dx = (gaussian(np.random.uniform(-1, 1, size=shape[:2]),
-                   c[1], mode='reflect', truncate=3) * c[0]).astype(np.float32)
-    dy = (gaussian(np.random.uniform(-1, 1, size=shape[:2]),
-                   c[1], mode='reflect', truncate=3) * c[0]).astype(np.float32)
-    dx, dy = dx[..., np.newaxis], dy[..., np.newaxis]
-
-    x, y, z = np.meshgrid(np.arange(shape[1]), np.arange(shape[0]), np.arange(shape[2]))
-    indices = np.reshape(y + dy, (-1, 1)), np.reshape(x + dx, (-1, 1)), np.reshape(z, (-1, 1))
-    return np.clip(map_coordinates(image, indices, order=1, mode='reflect').reshape(shape), 0, 1) * 255
 
 
 
@@ -884,6 +710,7 @@ if args.corruption == 'all':
     d['Interference'] = interference
     d['Underwater'] = underwater
     d['Rain'] = rain
+    d['Compression'] = compression # jpeg compression
 else:
     d[args.corruption] = eval(args.corruption.lower())
 
